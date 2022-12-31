9b878e324ba698e8cd49648b8da79becbb605f26
anand kumar <aak905536.com>
2022-12-31 14:04:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useState, useCallback } from 'react';
+import Header from './components/Header';
+import Habits from './components/Habits';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [habits, setHabits] = useState([
+    { id: 1, name: 'Reading', count: 0 },
+    { id: 2, name: 'Running', count: 0 },
+    { id: 3, name: 'Coding', count: 0 },
+  ]);
+
+  const handleAdd = useCallback((name) => {
+    setHabits((habits) => [...habits, { id: Date.now(), name, count: 0 }]);
+  }, []);
+
+  const handleIncrement = useCallback((habit) => {
+    setHabits((habits) =>
+      habits.map((item) => {
+        if (item.id === habit.id) {
+          return { ...habit, count: habit.count + 1 };
+        }
+        return item;
+      })
+    );
+  }, []);
+
+  const handleDecrement = useCallback((habit) => {
+    setHabits((habits) =>
+      habits.map((item) => {
+        if (item.count > 0 && item.id === habit.id) {
+          const count = habit.count - 1;
+          return { ...habit, count: count < 0 ? 0 : count };
+        }
+        return item;
+      })
+    );
+  }, []);
+
+  const handleDelete = useCallback((habit) => {
+    setHabits((habits) => habits.filter((item) => item.id !== habit.id));
+  }, []);
+
+  const handleClickReset = useCallback(() => {
+    setHabits((habits) =>
+      habits.map((habit) => {
+        if (habit.count !== 0) {
+          return { ...habit, count: 0 };
+        }
+        return habit;
+      })
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header totalCount={habits.filter((habit) => habit.count !== 0).length} />
+      <Habits
+        habits={habits}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        onDelete={handleDelete}
+        onAdd={handleAdd}
+        onReset={handleClickReset}
+      />
     </div>
   );
-}
+};
 
 export default App;
